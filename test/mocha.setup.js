2a9cbfcb7978fac499c8082d14420444cc0539d5
Tobias <tobias.pruegner@posteo.de>
2026-09-02 09:57:41 +0200

    test: force CommonJS module mode for ts-node mocha

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -6,6 +6,13 @@ process.env.TS_NODE_IGNORE_WARNINGS = "TRUE";
 process.env.TS_NODE_PROJECT = "tsconfig.json";
 // Make ts-node respect the "include" key in tsconfig.json
 process.env.TS_NODE_FILES = "TRUE";
+// Mocha 11 loads TypeScript test files through its ESM-aware loader on Node 22.
+// Keep ts-node in CommonJS mode for tests so extensionless local imports continue
+// to resolve exactly like they do in the existing adapter test suite.
+process.env.TS_NODE_COMPILER_OPTIONS = JSON.stringify({
+	module: "CommonJS",
+	moduleResolution: "node",
+});
 
 // Don't silently swallow unhandled rejections
 process.on("unhandledRejection", (e) => {
